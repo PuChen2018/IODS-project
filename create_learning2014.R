ed36810e2abb606dd72bd50192d46d332061c87a
Chen <puchen@lm1-945-24121.pc.helsinki.fi>
2021-11-09 13:29:31 +0200

    ignore the delelted file

--- create_learning2014.R
+++
@@ -1,5 +0,0 @@
-#Author: Pu Chen, 09112021, exercise code for week 2 homework
-learning2014<-read.table("https://www.mv.helsinki.fi/home/kvehkala/JYTmooc/JYTOPKYS3-data.txt",sep = "\t",header=TRUE)
-dim(learning2014)
-str(learning2014)
-#'data.frame':	183 obs. of  60 variables:
